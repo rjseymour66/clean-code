b01e8b17e61e70e6b8400e68c04b0f931b60f6f3
RJS66 <rjs.github.com>
2020-09-26 13:11:47 -0400

    maven structure

---
+++ src/main/java/com/cleancode/chapters/App.java
@@ -0,0 +1,13 @@
+package com.cleancode.chapters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
